ae330c82f74afda3903ebc2230d862be9ff88694
Matt Thomas <matt@betweenbrain.com>
2013-06-07 13:58:35 -0400

    Expand functionality to add support for the new module capabilities of the Joomla Ajax Interface

--- tmpl/default.php
+++ tmpl/default.php
@@ -6,12 +6,12 @@
  * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
  * Support    https://github.com/betweenbrain/
  * Copyright  Copyright (C) 2013 betweenbrain llc. All Rights Reserved.
- * License    GNU GPL v3 or later
+ * License    GNU General Public License version 2, or later.
  */
 
 ?>
-<form action="submit.php" method="post" id="timeForm">
-	<input type="text" name="dataInput">
+<form>
+	<input type="text" name="data">
 	<input type="submit" class="add" value="Add Value" />
 	<input type="submit" class="delete" value="Delete Value" />
 	<input type="submit" class="destroy" value="Destroy Session" />
